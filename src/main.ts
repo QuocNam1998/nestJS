353d3d86974abf4b19427723c269ff547abb95bf
CODE88\David.Dang <david.dang@code88.asia>
2026-05-08 14:07:04 +0700

    feat: add DTO and global pipe validation for tasks module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 1998);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips unknown fields automatically
+      forbidNonWhitelisted: true, // returns 400 if unknown fields sent
+      transform: true, // auto-transforms payloads to DTO class instances
+    }),
+  );
+  const port = process.env.PORT ?? 1998;
+  await app.listen(process.env.PORT ?? 1998, () => {
+    console.log(`App is listening on port: ${port}`);
+  });
 }
-bootstrap(); //set up the application and start listening on the specified port (default is 3000)
+void bootstrap(); //set up the application and start listening on the specified port (default is 3000)
